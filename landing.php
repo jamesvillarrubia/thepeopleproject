e4c4172360e989b9265fc597337c200465743dc4
jamesvillarrubia <james.villarrubia@gmail.com>
2015-04-26 22:33:01 -0400

    updates to launch

--- landing.php
+++ landing.php
@@ -60,12 +60,48 @@ echo $view->header;?>
     <!-- Header -->
     <header class="pd-v-xl bg-black tx-white">
     	<div class="colorizer"></div>
-        <div class="container pd-v-xl">
+        <div class="container pd-v-xl" style="
+		    position: relative;
+		    z-index: 1;
+		">
+        <div class="col-md-6 logo-wrapper">
+        	<img src="images/white_logo.png" class="img-responsive logo" style="">
+        </div>
+        <div class="col-md-6">
             <div class="intro-text text-center">
-                <div class="welcome-line">Hello & Welcome</div>
-                <div class="title-line">THE <span style="font-weight: 500">PEOPLE</span> PROJECT</div>
-                <a href="http://beta.peopleproject.us"><span class="dtran bg-none tx-hv-black bg-hv-white tx-white pd-v-xs pd-h-sm h4 bd-white bd-sm">SEE IT FOR YOURSELF</span></a>
-            </div>
+            	<div class="" style="
+				    padding: 17px 13px;
+				    text-transform: uppercase;
+				    font-size: 20px;
+				">
+            		Sign up to hear more:
+            	</div>
+	            <!-- Begin MailChimp Signup Form -->
+				<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+				<style type="text/css">
+					#mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; }
+					/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+					   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+				</style>
+				<div id="mc_embed_signup">
+				<form action="//peopleproject.us10.list-manage.com/subscribe/post?u=9d96f750e45ed0698a8bdd8fc&amp;id=87486be2a2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+					
+				<div class="mc-field-group">
+					<input type="email" value="" placeholder="EMAIL" name="EMAIL" class="required email" id="mce-EMAIL">
+				</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_9d96f750e45ed0698a8bdd8fc_87486be2a2" tabindex="-1" value=""></div>
+				    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				    </div>
+				</form>
+				</div>
+				<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+				<!--End mc_embed_signup-->
+			 </div>
         </div>
     </header>
 
